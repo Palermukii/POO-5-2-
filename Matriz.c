163135fbad2a703333be16500567d2f519e9cdc9
Tomas Palermo <109637082+Palermukii@users.noreply.github.com>
2024-04-17 11:42:55 -0300

    Update Matriz.c

--- Matriz.c
+++ Matriz.c
@@ -2,26 +2,31 @@
 #include <stdlib.h>
 int main ()
 {
- int A[3][3];
- printf("Vaya ingresando los numeros: ");
- for(int i = 0; i < 3; i++) {
-         for(int j = 0; j < 3; j++) {
+	int filas;
+	int columnas;
+	printf("Ingrese la cantidad de filas: ");
+	scanf("%d", &filas);
+ 	printf("Ingrese la cantidad de columnas: ");
+	scanf("%d", &columnas);
+ 	int A[filas][columnas];
+ 	printf("Vaya ingresando los numeros: ");
+ 
+ 
+ 	for(int i = 0; i < filas; i++) { // Ingresar numero por numero
+         for(int j = 0; j < columnas; j++) {
                  printf("Ingrese A[%d][%d]: ",i,j);
                  scanf("%d", &A[i][j]);
                  }
          
          }
-  for(int i = 0; i < 3; i++) {
+  	for(int i = 0; i < filas; i++) { // Muestra los numeros en pantalla
           printf("\n");
-         for(int j = 0; j < 3; j++) {
+         for(int j = 0; j < columnas; j++) {
                  printf("%d\t",A[i][j]);
                  }
          
          }
 
-
-
-
-
+printf("\n");
 system("pause");
 }
