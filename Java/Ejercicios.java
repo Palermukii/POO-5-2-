ecec2a8e876d55fe0366d842c59ff17a3f85a6ec
Tomas Palermo <109637082+Palermukii@users.noreply.github.com>
2024-09-02 16:06:52 -0300

    Update Ejercicios.java

--- Java/Ejercicios.java
+++ Java/Ejercicios.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;  // Import the Scanner class
 
-class Ejercicios {
+public class Ejercicios {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         System.out.println("Elija un ejercicio");
@@ -12,7 +12,18 @@ class Ejercicios {
             case 2:
             ejercicio2();
             break;
+            case 3:
+            ejercicio3();
+            break;
+            case 4:
+            ejercicio4();
+            break;
+            case 5:
+            ejercicio5();
+            break;
         }
+    myObj.close();
+
     }
 
     private static void ejercicio1() {
@@ -54,6 +65,89 @@ class Ejercicios {
                 }
             }
             System.out.println("\n");
+        }
+    }
+    private static void ejercicio3() {
+        Scanner myObj2 = new Scanner(System.in);
+        while (true) {
+            System.out.print("Ingresa un número: ");
+            int num = myObj2.nextInt();
+            if (num > 100 && !primo(num)) {
+                System.out.println("Es mayor a 100 y primo");
+                break;
+            } else {
+                System.out.println("No es un número mayor a 100 o no es primo");
+            }
+        }
+        myObj2.close();
+    }
+
+    private static int suma(int a, int b){
+        return a + b;
+    }
+    private static int resta(int a, int b){
+        return a - b;
+    }
+    private static int multiplicacion(int a, int b){
+        return a * b;
+    }
+    private static double division(int a, int b){
+        return a / b;
+    }
+
+
+    private static void ejercicio4() {
+        Scanner myObj3 = new Scanner(System.in);
+        while (true) {
+            System.out.print("Ingresa el primer número: ");
+            int a = myObj3.nextInt();
+
+            System.out.print("Ingresa el segundo número: ");
+            int b = myObj3.nextInt();
+
+            System.out.println("Ingrese que tipo de operación aritmética quiere realizar: + (suma), - (resta), * (multiplicación), / (división): ");
+            char oper = myObj3.next().charAt(0);
+
+            switch (oper) {
+                case '+':
+                    System.out.println("La suma de ambos números es " + suma(a, b));
+                    break;
+                case '-':
+                    System.out.println("La resta de ambos números es " + resta(a, b));
+                    break;
+                case '*':
+                    System.out.println("La multiplicación de ambos números es " + multiplicacion(a, b));
+                    break;
+                case '/':
+                    if (b != 0) {
+                        System.out.println("La división de ambos números es " + division(a, b));
+                    } else {
+                        System.out.println("Error: No se puede dividir por cero.");
+                    }
+                    break;
+                default:
+                    System.out.println("Ingrese un valor válido");
+                    break;
+            }
+            System.out.println("¿Desea continuar? (s/n): ");
+            char continuar = myObj3.next().charAt(0);
+            if (continuar == 'n' || continuar == 'N') {
+                break;
+            }
+        }
+        myObj3.close();
+    }
+    private static void ejercicio5() {
+        int nums[] = {1, 3 ,5, 10};
+        System.out.println("Los numeros de la lista son: ");
+        for(int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+        System.out.println("Los siguientes numeros son primos");
+        for(int n : nums) {
+            if(!primo(n)) {
+                System.out.println(n + " es primo");
+            }
         }
     }
 }
